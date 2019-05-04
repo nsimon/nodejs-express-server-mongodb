b61342a9ff5c44b48e0a5120b85d733c2c45811f
Neil Simon <nsimon@solidcode.com>
2019-05-04 14:30:17 -0600

    Now populating all director dropdowns with director names

--- static/content/admin.js
+++ static/content/admin.js
@@ -1,22 +1,97 @@
 // admin.js
 
+// How to extract a selected value from a dropdown:
+//   var employeeFirstCommaLast = $("#supervisorStaffDropdown").find("option:selected").text ();  // ex: Simon, Neil
+
+// How to hook a "changed" select:
+//   function hookSupervisorStaffDropdown ()
+//       {
+//       // HOOK: #supervisorStaffDropdown
+//       $("#supervisorStaffDropdown").change (function ()
+//           {
+//           }
+//       }
+
 $(function ()
     {
-    var tmpl;  // Receives the contents of "/templates/admin_page.div"
-
-    // Initialize page
-    var initPage = function ()
+    // 1. GET /templates/admin_page.div, insert into <body>
+    $.ajax (
         {
-        // Load the HTML template
-        $.get ("/templates/admin_page.div", function (d)
+        url:   "/templates/admin_page.div",  // html to be inserted into "body"
+        type:  "GET",
+        error: function (xhr, status, error)
             {
-            tmpl = d;
-            });
- 
-        // When AJAX calls are complete, parse the template replacing mustache tags with vars
-        $(document).ajaxStop (function ()
+            alert ("ajax() error on: GET /templates/admin_page.div" + "\n" +
+                   "status: " + status + "\n" +
+                   "error: "  + error);
+            },
+        success: function (admin_page_div)
             {
-            $("body").html (tmpl);
-            });
-        }();
+            // alert ("ajax() success: inserting admin_page.div into <body>");
+
+            // insert the admin_page.div
+            $("body").html (admin_page_div);
+            }
+        });
+
+    // 2. GET /v1/directors.json, insert into 3 "director" dropdowns
+    $.ajax (
+        {
+        url:   "/v1/directors.json",
+        type:  "GET",
+        error: function (xhr, status, error)
+            {
+            alert ("ajax() error on: GET /v1/directors.json" + "\n" +
+                   "status: " + status + "\n" +
+                   "error: "  + error);
+            },
+        success: function (data)
+            {
+            // alert ("ajax() success: data: " + JSON.stringify (data));
+            // (ex) data = { "rc": rc, "message": message, "data": { "directors": director_list }};
+
+            var rc            = data.rc;
+            var message       = data.message;
+            var directors     = data.data.directors;
+            var options       = "";
+            var director_name = "";
+
+            // alert ("rc: "        + rc      + "\n" +
+            //        "message: "   + message + "\n" +
+            //        "directors: " + directors);
+
+            // Hardcode 1st dropdown option
+            options += "<option disabled selected>Choose director...</option>";
+
+            // Get each director name
+            for (var i = 0; i < directors.length; i++)
+                {
+                // ex: Scorsese
+                director_name = directors [i].name;
+
+                // alert (director_name);
+
+                // ex: <option>Scorsese</option>
+                options += "<option value='" + director_name + "'>" + director_name + "</option>";
+                }
+
+            // alert ("options: " + options);
+
+            // Insert into director dropdowns
+            $("#admin-delete-director-director").empty ();          // clear
+            $("#admin-delete-director-director").append (options);  // append options
+            $("#admin-add-movie-director").empty ();
+            $("#admin-add-movie-director").append (options);
+            $("#admin-delete-movie-director").empty ();
+            $("#admin-delete-movie-director").append (options);
+            }
+        });
+
+    // 3. Create triggers:
+    //    . add-movie-director-dropdown, delete-movie-director-dropdown
+    //      - to populate associated movie dropdown
+    //    . all buttons
+    //      - to issue API calls
+    //    . NOTE: initially, test each trigger with alert()
     });
+
